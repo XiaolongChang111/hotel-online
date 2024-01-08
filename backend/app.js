d2430cb4a36ea8a20df65fb2bbbb7c1650390663
XiaolongChang111 <1707308100@qq.com>
2024-01-08 20:53:21 +0800

    change any route to index.html

--- backend/app.js
+++ backend/app.js
@@ -33,7 +33,7 @@ mongoose.connect(process.env.MONGODB_URL || 'mongodb+srv://asdf:asdf@test.kgyv4w
 
 
 app.use(function(req, res) {
-    res.sendFile(path.join(__dirname, '../build/index.html'));
+    res.sendFile(path.join(__dirname, './build/index.html'));
 })
 
 let sockets = {};
